88395cbbad4f04c1a0f1a656a644a43e1ea0a1ae
Luffy251 <shailendrapratap5522@gmail.com>
2026-04-13 16:48:55 +0530

    fix: api base url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const API = "https://psychin-backend.onrender.com";
+
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: API,
 });
 
 api.interceptors.request.use((config) => {
